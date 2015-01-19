d7949ad5bd980ab54a51a5548c5a10ad9a330b32
Mustafa Bilgic <mbilgic@iit.edu>
2015-01-19 12:10:06 -0600

    user study

--- structured_reading/user_study.py
+++ structured_reading/user_study.py
@@ -7,117 +7,79 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 import sys
 import os
-
-# Loading Data
-vct = exputil.get_vectorizer({'vectorizer':"tfidf", 'limit':None, 'min_size':None})
-# Sentence tokenizers
-sent_tk = nltk.data.load('tokenizers/punkt/english.pickle')
-
-def load_data(dataname, path):
-    import pickle
-
-    DATA_PKL = path + '/data.pkl'
-
-    if os.path.isfile(DATA_PKL):
-        vct, data = pickle.load(open(DATA_PKL, 'rb'))
-    else:
-        vct = exputil.get_vectorizer({'vectorizer':"tfidf", 'limit':None, 'min_size':None})
-        data = datautil.load_dataset(dataname, path, categories=None, rnd=5463, shuffle=True)
-        data.train.data = np.array(data.train.data, dtype=object)
-        data.test.data = np.array(data.test.data, dtype=object)
-        data.train.bow = vct.fit_transform(data.train.data)
-        data.test.bow = vct.transform(data.test.data)
-        pickle.dump((vct, data), open(DATA_PKL, 'wb'))
-
-    return data, vct
-
-# Get the sentences for testing
-def _sentences(docs, doc_labels, sent_tk):
-    data = []
-    true_labels = []
-    sent = sent_tk.tokenize_sents(docs)
-    for sentences, doc_label in zip(sent, doc_labels):
-        data.extend(sentences)
-        true_labels.extend([doc_label] * len(sentences))
-    return data, np.array(true_labels)
-
-def load_study(filename):
-    f = open(filename)
-    with f:
-        lines = f.readlines() 
-    data =  [l.strip().split("\t") for l in lines[1:]] #discard first line
-    data = np.array(data, dtype=object)
-    return data
-
-IMDB_DATA = "C:\\Users\\mbilgic\\Desktop\\aclIMDB"
-
-# Load the dataset
-print "Loading the data"
-imdb, vct = load_data('imdb', IMDB_DATA)
+from sklearn.feature_extraction.text import  TfidfVectorizer
+import pickle
+from sklearn.linear_model import LogisticRegression
 
 
-# Load the dictionary from dist
-
-with open(IMDB_DATA+"\\dictionary2.csv") as f:
+with open("imdb-vocab-annotated.txt") as f:
     lines = f.readlines()
     vocab = [l.strip() for l in lines]
 
-from sklearn.feature_extraction.text import  TfidfVectorizer
+print "Dictionary size: %d" %len(vocab)
+#print vocab
+
+# Loading Data
+print "Loading the data"
 vct = TfidfVectorizer(encoding='ISO-8859-1', min_df=5, max_df=1.0, binary=False, vocabulary=vocab)
-vct.fit(imdb.train.data)
 
-# Get the snippets from the saved file
-docs_study = load_study('rnd_imdb.txt')
-lbl = [float(d[0]) for d in docs_study]
-snippets = [d[1] for d in docs_study]
-snip_bow = vct.transform(snippets)
+if os.path.isfile("imdb-data.pkl"):
+    data = pickle.load(open("imdb-data.pkl", 'rb'))
+else:
+    data = datautil.load_dataset("imdb", "C:\\Users\\mbilgic\\Desktop\\aclIMDB", categories=None, rnd=5463, shuffle=True)
+    data.train.data = np.array(data.train.data, dtype=object)
+    data.test.data = np.array(data.test.data, dtype=object)
+    pickle.dump(data, open("imdb-data.pkl", 'wb'))
 
-## Get the expert data and snippets
-#exp_sent, exp_lbl = _sentences(imdb.test.data, imdb.test.target, sent_tk)
-#exp_sent_bow = vct.transform(exp_sent)
-#print "Sentences:", len(exp_sent), len(exp_lbl)
+print "Fitting the vectorizer"
+data.test.bow = vct.fit_transform(data.test.data)
 
-from sklearn.linear_model import LogisticRegression
-expert = LogisticRegression('l1', C=5)
+# Fit the expert
+print "Training the expert"
+expert = LogisticRegression('l2', C=1)
+expert.fit(data.test.bow, data.test.target)
 
-#from sklearn.tree import DecisionTreeClassifier
-#expert = DecisionTreeClassifier()
+terms = np.array(vct.get_feature_names())
 
-#from sklearn.naive_bayes import MultinomialNB
-#expert = MultinomialNB()
+coefs = expert.coef_[0]
 
-#expert.fit(imdb.test.bow, imdb.test.target)
-expert.fit(vct.transform(imdb.test.data), imdb.test.target)
-pred = expert.predict(snip_bow)
-prob = expert.predict_proba(snip_bow)
+for t in range(len(terms)):
+    print "%s\t%0.4f" %(terms[t], coefs[t])
 
-print "num nonzero %d" %len(np.nonzero(expert.coef_[0])[0])
+exit(0)
 
-#exit(0)
+data.train.bow = vct.transform(data.train.data)
 
-def print_top_terms(model, terms, n=20):
-    print '\nTop Coefficients'
-    coef = model.coef_[0]
-    srted = np.argsort(coef)
-    topi = srted[::-1][:n]
-    boti = srted[:n]
-    #"\n".join('%s\t%.2f' % (names[j], coef[j]) for j in np.argsort(coef)[::-1] if coef[j] != 0)
-    print 'Class-0 Terms:\n' + '\n'.join('%s (%.2f)' % (n, c) for n, c in zip(terms[topi], coef[topi]) if c != 0)
-    print '\nClass-1 Terms:\n' + '\n'.join('%s (%.2f)' % (n, c) for n, c in zip(terms[boti], coef[boti]) if c != 0)
-    print '\nintercept=%g' % model.intercept_
-    
-#terms = np.array(vct.get_feature_names())
+y_pred = expert.predict(data.train.bow)
 
+from sklearn.metrics import accuracy_score
+
+print "Accuracy %0.4f" %accuracy_score(data.train.target, y_pred)
+
+# full documents
+
+for doc in range(data.train.bow.shape[0]):    
+    if len(data.train.bow[doc].indices) < 1:         
+        print
+        print data.train.data[doc]  
+        raw_input()
+exit(0)
 
 
-#print "printing terms to a file"
+# Sentence tokenizers
+sent_tk = nltk.data.load('tokenizers/punkt/english.pickle')
 
-#with open(IMDB_DATA+"\\vocab.csv", "w") as f:
-#    for i in range(len(terms)):
-#        f.write(str(i)+","+terms[i].encode("utf-8")+","+str(expert.coef_[0][i])+"\n")
+n = 1000
+rnd = np.random.RandomState()
+rnd_docs = rnd.choice(len(data.train.target), size = n, replace = False)
+sentences = sent_tk.tokenize_sents(data.train.data[rnd_docs])
 
-#exit(0)
-
-for t, p in zip(pred,prob):
-    l = t if (1- p.max()) < .46 else 2 
-    print l
+# individual sentences
+for doc in sentences:
+    for sent in doc:
+        x = vct.transform([sent])
+        max_prob = max(expert.predict_proba(x)[0])
+        if max_prob < 0.55:            
+            print
+            print sent        
+            raw_input()
