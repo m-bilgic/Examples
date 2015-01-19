88c52872c2799ac45c869e1ad313abbd12dc4057
m-bilgic <mbilgic@iit.edu>
2015-01-19 09:59:01 -0600

    user study

--- structured_reading/user_study.py
+++ structured_reading/user_study.py
@@ -55,6 +55,17 @@ IMDB_DATA = "C:\\Users\\mbilgic\\Desktop\\aclIMDB"
 print "Loading the data"
 imdb, vct = load_data('imdb', IMDB_DATA)
 
+
+# Load the dictionary from dist
+
+with open(IMDB_DATA+"\\dictionary2.csv") as f:
+    lines = f.readlines()
+    vocab = [l.strip() for l in lines]
+
+from sklearn.feature_extraction.text import  TfidfVectorizer
+vct = TfidfVectorizer(encoding='ISO-8859-1', min_df=5, max_df=1.0, binary=False, vocabulary=vocab)
+vct.fit(imdb.train.data)
+
 # Get the snippets from the saved file
 docs_study = load_study('rnd_imdb.txt')
 lbl = [float(d[0]) for d in docs_study]
@@ -67,7 +78,7 @@ snip_bow = vct.transform(snippets)
 #print "Sentences:", len(exp_sent), len(exp_lbl)
 
 from sklearn.linear_model import LogisticRegression
-expert = LogisticRegression('l1', C=0.1)
+expert = LogisticRegression('l1', C=5)
 
 #from sklearn.tree import DecisionTreeClassifier
 #expert = DecisionTreeClassifier()
@@ -75,10 +86,38 @@ expert = LogisticRegression('l1', C=0.1)
 #from sklearn.naive_bayes import MultinomialNB
 #expert = MultinomialNB()
 
-expert.fit(imdb.test.bow, imdb.test.target)
+#expert.fit(imdb.test.bow, imdb.test.target)
+expert.fit(vct.transform(imdb.test.data), imdb.test.target)
 pred = expert.predict(snip_bow)
 prob = expert.predict_proba(snip_bow)
 
+print "num nonzero %d" %len(np.nonzero(expert.coef_[0])[0])
+
+#exit(0)
+
+def print_top_terms(model, terms, n=20):
+    print '\nTop Coefficients'
+    coef = model.coef_[0]
+    srted = np.argsort(coef)
+    topi = srted[::-1][:n]
+    boti = srted[:n]
+    #"\n".join('%s\t%.2f' % (names[j], coef[j]) for j in np.argsort(coef)[::-1] if coef[j] != 0)
+    print 'Class-0 Terms:\n' + '\n'.join('%s (%.2f)' % (n, c) for n, c in zip(terms[topi], coef[topi]) if c != 0)
+    print '\nClass-1 Terms:\n' + '\n'.join('%s (%.2f)' % (n, c) for n, c in zip(terms[boti], coef[boti]) if c != 0)
+    print '\nintercept=%g' % model.intercept_
+    
+#terms = np.array(vct.get_feature_names())
+
+
+
+#print "printing terms to a file"
+
+#with open(IMDB_DATA+"\\vocab.csv", "w") as f:
+#    for i in range(len(terms)):
+#        f.write(str(i)+","+terms[i].encode("utf-8")+","+str(expert.coef_[0][i])+"\n")
+
+#exit(0)
+
 for t, p in zip(pred,prob):
-    l = t if (1- p.max()) < .45 else 2 
+    l = t if (1- p.max()) < .46 else 2 
     print l
